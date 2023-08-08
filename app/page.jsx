21baf59a5e2bf7f73bed5682b61e29d319bb1989
AyjayJr <ayjayjr@hotmail.com>
2023-08-07 21:47:09 -0400

    added two new components that will contribute to functionality

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Nav />
       <Sidebar />
+      <div className='w-full'>
+
+      </div>
     </main>
   )
 }
---
+++ components/MarkdownEditor.jsx
@@ -0,0 +1,17 @@
+import ReactMarkdown from "react-markdown"
+
+const MarkdownEditor = ({ content, handleChange }) => {
+	return (
+		<div>
+			<textarea
+				className="w-full p-2 border rounded"
+				value={content}
+				onChange={handleChange}
+				rows={10}
+			/>
+			<ReactMarkdown>{content}</ReactMarkdown>
+		</div>
+	)
+}
+
+export default MarkdownEditor
