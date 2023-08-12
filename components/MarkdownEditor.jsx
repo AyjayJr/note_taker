318142916759387eefce24b58e571f4c08c40d8f
AyjayJr <48783536+AyjayJr@users.noreply.github.com>
2023-08-12 00:58:26 -0400

    made markdown component span full width of parent div

--- components/MarkdownEditor.jsx
+++ components/MarkdownEditor.jsx
@@ -7,13 +7,13 @@ const MarkdownEditor = ({ content, handleChange }) => {
 	return (
 		<div className='flex h-full flex-row gap-10'>
 			<textarea
-				className='flex-1 w-1/2 h-full p-3 border rounded bg-accent resize-none'
+				className='flex-1 h-full p-3 border rounded bg-accent resize-none'
 				value={content}
 				onChange={handleChange}
 			/>
-			<div className="flex-1 w-1/2 p-3 border border-black rounded">
+			<div className="flex-1 p-3 border border-black rounded">
 				<ReactMarkdown
-					className='prose prose-invert'
+					className='min-w-full prose prose-invert'
 					remarkPlugins={[remarkBreaks, rehypeHighlight, remarkGfm]}
 				>
 					{content}
